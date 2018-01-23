b534e57c4df9786ed5b992bcbc6a99b8f8b80f44
Ryan Lessley <ryan.lessley@revature.com>
2018-01-23 10:50:41 -0500

    day 2

--- Week1/_InClassExamples/180121_Java/src/com/revature/day1/scopes/Driver.java
+++ Week1/_InClassExamples/180121_Java/src/com/revature/day1/scopes/Driver.java
@@ -3,6 +3,8 @@ package com.revature.day1.scopes;
 public class Driver {
 
 	public static void main(String[] args) {
+		System.out.println("=====\nAmount of persons: " + Person.personCount + "\n=====");
+		
 		//When instantiating a class you need to follow the below format:
 		//Reference = assignment
 		//ClassName varName = new ClassName()
@@ -13,6 +15,34 @@ public class Driver {
 		
 		System.out.println("Name: " + p.name);
 		System.out.println("Age: " + p.age);
+		
+		p.someMethod();
+		
+		System.out.println("=====\nAmount of persons: " + Person.personCount + "\n=====");
+		
+		Person p2 = new Person();
+		Person p3 = new Person();
+		Person p4 = new Person();
+		
+		System.out.println("=====\nAmount of persons: " + Person.personCount + "\n=====");
+		
+		p4 = new Person();
+		p4 = new Person();
+		p4 = new Person();
+		
+		System.gc(); //This is a REQUEST for garbage collection, not a force.
+		
+		//Tell the main thread to wait for 1 second.
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("=====\nAmount of persons: " + Person.personCount + "\n=====");
+
+
 	}
 
 }
---
+++ Week1/_InClassExamples/180121_Java/src/com/revature/day2/accessMods2/Driver.java
@@ -0,0 +1,41 @@
+package com.revature.day2.accessMods2;
+
+import com.revature.day2.accessmods.Silence;
+
+/*
+ * In order to access classes from other packages, you need to IMPORT them.
+ */
+//NOTE: use ctrl+shift+o to bring in required packages quickly.
+
+public class Driver extends Silence{
+
+	public static void main(String[] args) {
+		
+		Silence silence = new Silence();
+		System.out.println("====ACCESS VIA METHOD IN CLASS====");
+		silence.outputVariables();
+		
+		System.out.println("====ACCESS VIA METHOD OUTSIDE CLASS, OUTSIDE PACKAGE====");
+		/*
+		 * When outside of the package, and not a subclass, you cannot access anything
+		 * unless it is public.
+		 */
+		//System.out.println(silence.priv); //Cannot access private from outside class
+		//System.out.println(silence.def); //Cannot access
+		//System.out.println(silence.prot); //Cannot access
+		System.out.println(silence.pub);
+		
+		/*
+		 * Using an instance of a class that EXTENDS the silence class.
+		 * I get access to all PUBLIC and PROTECTED fields and methods.
+		 * defaults are omitted however.
+		 */
+		Driver d = new Driver();
+		//System.out.println(d.priv); //Not valid
+		//System.out.println(d.def);  //Not valid
+		System.out.println(d.prot);
+		System.out.println(d.pub);
+		
+	}
+
+}
