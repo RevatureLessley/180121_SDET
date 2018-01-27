d583dd2e4985cf04573e28487323cac9375b8cc6
harish <harishkumarchandra@gamil.com>
2018-01-27 16:49:50 -0500

    add

--- Week1/MiniProject/harish/src/com/HKBank/code/Bank.java
+++ Week1/MiniProject/harish/src/com/HKBank/code/Bank.java
@@ -7,6 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashSet;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -14,40 +15,90 @@ public class Bank {
 	
 	// Runs Bank console application
 	public void run() throws IOException {
+		
+		//Load user data
 		Set<User> users = loadUserData();
+		
 		boolean session = true;
 		while(session) {
 			switch(homePage()) {
 			case 1:
-				//
-				break;
-			case 2:
-				//
-				for(User p: users) {
-					System.out.println(p);
+				//Login Page for Administrator and User
+				switch(loginPage(users)) {
+				case 0:
+					//Administrator Screen
+					System.out.println("Welcome Administrator\n");
+					AdminPage(users);
+					break;
+				case 1:
+					//User Screen
+					System.out.println("Welcome User");
+					break;
+				default:
+					System.in.read();
 				}
 				break;
-			case 3:
+			case 2:
 				//New User Page
+				System.out.println("Welcome New User\n");
 				users.add(newUser());		
 				break;
-			case 4:
+			case 3:
 				//Exit from bank
-				System.out.println("Thank you for Banking with us");
+				System.out.println("Thank you for Banking with us\n");
 				session = false;
 				break;
 			default:
 				//Out of range in Menu
 				System.out.println("Please select again\n");
 			}
-			saveUserData(users);
+		
+		//Save user data
+		saveUserData(users);
 		}
 		
 	}
 	
+	//Administrator Page to Approve or Reject new accounts
+	public void AdminPage(Set<User> users) throws IOException {
+		Iterator<User> it = users.iterator();
+		while (it.hasNext()) {
+		     User p = it.next();
+			 if(p.getStatus().equals("notApproved")) {
+				System.out.println("==================================");
+				System.out.println(p.getUserName());
+				System.out.println(	"1.Approve\n"+
+									"2.Reject\n");
+				System.out.println("==================================");
+				Scanner i = new Scanner(System.in);
+				int action = 0;
+				while(true) {
+					try {
+						System.out.print("Enter your selection: ");
+						action= i.nextInt();
+						System.out.println("Your selection is: "+action+"\n");
+					}catch(InputMismatchException e) {
+						e.printStackTrace();
+						System.out.println("\n Enter numbers only \n");
+					}
+				    if(action == 1) {
+				    	p.setStatus("Approved");
+				    	break;
+				    }
+				    if(action == 2) {
+				    	it.remove();
+				    	break;
+				    }
+				}
+			}
+		}
+		System.out.println("No more new account waiting for Approval\n");
+		System.in.read();
+
+	}
+
 	//Load user data at start
-	//Load User data from file
-public Set<User> loadUserData() throws IOException{
+	public Set<User> loadUserData() throws IOException{
 		ObjectInputStream ois = null;
 		try {
 			ois = new ObjectInputStream(new FileInputStream("src/com/HKBank/files/usersData.ser"));
@@ -62,16 +113,14 @@ public Set<User> loadUserData() throws IOException{
 		return null;
 	}
 	
-	
 	//Home Page of Bank
 	public int homePage() {
 			//Welcome screen and Menu option
 			int selection = 0;
 			System.out.println("======= Welcome to HKBank ========");
-			System.out.println(	"1.Admin login\n"+
-								"2.User login\n"+
-								"3.New User\n"+
-								"4.Exit");
+			System.out.println(	"1.Login\n"+
+								"2.New User\n"+
+								"3.Exit");
 			System.out.println("==================================");
 			System.out.print("Enter your selection: ");
 			Scanner i = new Scanner(System.in);
@@ -84,7 +133,32 @@ public Set<User> loadUserData() throws IOException{
 			}
 			return selection;
 		}
+	
+	//Login Page
+	int loginPage(Set<User> u) {
+		Scanner i = new Scanner(System.in);
+		System.out.print("Enter User Name: ");
+		String userName = i.nextLine();
+		System.out.print("Enter password: ");
+		String password = i.nextLine();
 		
+		if(userName.equals("Admin")&&password.equals("Admin")) {
+			return 0;
+		}
+		for (User p: u) {
+			if(userName.equals(p.getUserName())&&password.equals(p.getPassword())){
+				if((p.getStatus().equals("Approved"))) {
+					return 1;
+				}else {
+					System.out.println("Sorry your account is not Approved yet\n");
+					return -1;
+				}
+			}
+		}
+		System.out.println("Invaid login. Try again or create new account\n");		
+		return -1;
+	}
+	
 	//New user registration Page
 	public User newUser() throws IOException {
 		Scanner i = new Scanner(System.in);
@@ -97,7 +171,6 @@ public Set<User> loadUserData() throws IOException{
 		System.in.read();
 		return new User(userName, password, 0.0, "notApproved");
 		}	
-	
 	
 	//Save user data before ending application
 	public void saveUserData(Set<User> save) throws IOException {
