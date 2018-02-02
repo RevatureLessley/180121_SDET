2169045f0edcb5c4119c38e9e8da51e0446fe485
Lynda Ademola <lyndaxj9@gmail.com>
2018-02-02 17:43:18 -0500

    added Connection class and can now connect to database

--- Week1/MiniProject/src/com/miniproject/minisaver/Driver.java
+++ Week1/MiniProject/src/com/miniproject/minisaver/Driver.java
@@ -1,9 +1,20 @@
 package com.miniproject.minisaver;
 
+import java.sql.Connection;
+
+import com.miniproject.util.Connections;
+
 public class Driver {
 	
 	public static void main(String[] args) {
 		MiniSaver miniSaver = new MiniSaver();
 		miniSaver.runProgram();
+		
+		try(Connection conn = Connections.getConnection()){
+			System.out.println("SUCCESS!");
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("FAILURE");
+		}
 	}
 }
---
+++ Week1/MiniProject/src/com/miniproject/util/Connections.java
@@ -0,0 +1,22 @@
+package com.miniproject.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connections {
+	public static Connection getConnection() throws SQLException {
+		String[] props = System.getenv("MPDBPROPS").split(";");
+		try {
+			Class.forName(props[0]);
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return DriverManager.getConnection(
+				props[1],
+				props[2],
+				props[3]
+		);
+	}
+}
