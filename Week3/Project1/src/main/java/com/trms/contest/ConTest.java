3dc83ecf70117b57a2eb02713f88e6ad53967227
Lynda Ademola <lyndaxj9@gmail.com>
2018-02-11 01:21:31 -0500

    remade project files, tested connection to database

---
+++ Week3/Project1/src/main/java/com/trms/contest/ConTest.java
@@ -0,0 +1,45 @@
+package com.trms.contest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.trms.util.Connections;
+
+/**
+ * Servlet implementation class ConTest
+ */
+public class ConTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		try(Connection conn = Connections.getConnection()){
+			
+			out.println("<h2>Connection Success!</h2>"
+					);
+		} catch(SQLException e) {
+			e.printStackTrace();
+			out.println("<h2>Connection Fail!</h2>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
