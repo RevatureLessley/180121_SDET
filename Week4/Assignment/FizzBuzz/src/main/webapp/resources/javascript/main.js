99ab7be86e72b4147dfbc03e75d762bdcdac8790
harish <harishkumarchandra@gamil.com>
2018-02-14 17:50:29 -0500

    fizzbuzz done

--- Week4/Assignment/FizzBuzz/src/main/webapp/resources/javascript/main.js
+++ Week4/Assignment/FizzBuzz/src/main/webapp/resources/javascript/main.js
@@ -3,18 +3,20 @@ function genFizzBuzz(){
 	var start = document.getElementById("start").value;
 	var end = document.getElementById("end").value;
 	
+	//swap if start is bigger then end
 	if(start > end){
 		temp = start;
 		start = end;
 		end = temp;
 	}
 	
+	//check allowable range of start and end
 	if(start < 0 || end >100){
 		window.alert('Entered value is out of range, Please enter value between 0 and 100');
-		return true;
+		return false;
 	}
 	
-	//table = document.getElementById("theTable");
+	//Fizz Buzz Logic
 	for (var i=start; i <= end; i++)
 	{
 		var row = document.createElement('tr');
@@ -44,7 +46,21 @@ function genFizzBuzz(){
     	td2.appendChild(fb);
     	row.appendChild(td1);
     	row.appendChild(td2);
+    	row.setAttribute("id", "row");
     	document.getElementById("theTable").appendChild(row);
+    	
 	}
      
+}
+
+//clear table contents
+function clearTable(){
+	
+	var elmtTable = document.getElementById('theTable');
+	var tableRows = elmtTable.getElementsByTagName('tr');
+	var rowCount = tableRows.length;
+
+	for (var x=rowCount-1; x>0; x--) {
+	   elmtTable.removeChild(tableRows[x]);
+	}
 }
