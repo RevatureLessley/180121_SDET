549bed768f7dca6e589ff411b9886d8bfc20b04f
Ryan Lessley <ryan.lessley@revature.com>
2018-02-14 16:59:20 -0500

    week 4

---
+++ Week4/InclassExamples/180121_Week4_Javascript/src/main/webapp/resource/javascript/AJAX.js
@@ -0,0 +1,64 @@
+function sendAJAX(){
+	var xhr = new XMLHttpRequest();
+	/*
+	 * There exists 5 states of an XMLHttpRequest object.
+	 * 0 - Request is not configured
+	 * 		-We created our XMLHttpRequest object, but we have not configured it.
+	 * 1 - Request has been configured
+	 * 		-We have called the open(), but we have not called the send().
+	 * 2 - Request has been sent
+	 * 		-We have called send();
+	 * 3 - Request is being processed
+	 * 		-Communication with the server has been established.
+	 * 		-Yet we have not received the full response yet.
+	 * 4 - Request has been sent, and a proper response has been received back from server.
+	 * 		-Request/Response lifecycle is complete.
+	 */
+	xhr.onreadystatechange = function(){
+		console.log("READY STATE CHANGE: " + xhr.readyState);
+		if(xhr.readyState == 4 && xhr.status == 200){
+			/*
+			 * In the event of a successful request/response, as indicated by state = 4, and
+			 * status = 200, we will then take the data as XML.
+			 */
+			var xmlText = xhr.responseXML;
+			var response = xmlText.getElementsByTagName("employee");
+			//response is a collection of all employee tags.
+			var resultTable = document.getElementById("results");
+			//variable referencing our end table.
+			for(e in response){
+				var row = document.createElement("tr");
+				var td1 = document.createElement("td");
+				var td2 = document.createElement("td");
+				var td3 = document.createElement("td");
+				var td4 = document.createElement("td");
+				
+				td1.innerHTML = response[e].childNodes[0].innerHTML;
+				td2.innerHTML = response[e].childNodes[1].innerHTML;
+				td3.innerHTML = response[e].childNodes[2].innerHTML;
+				td4.innerHTML = response[e].childNodes[3].innerHTML;
+				
+				row.appendChild(td1);
+				row.appendChild(td2);
+				row.appendChild(td3);
+				row.appendChild(td4);
+				
+				resultTable.appendChild(row);
+			}
+		}else if(xhr.readyState == 4 && xhr.status!=200){
+			console.log(xhr.status);
+			document.getElementById("AJAXError").innerHTML=xhr.status;
+		}
+		/*
+		 * The open method is used to configure the actual XMLHttpRequest object.
+		 * We configure what kind of HTTP method we are using, and where it is going.
+		 * The parameters look like:
+		 * 	open(HTTPMETHOD, ENDPOINT, UseAsynchronous)
+		 * 			-note, UseAsynchronous is optional and will default to true.
+		 * 
+		 */
+
+	}
+	xhr.open("GET","GetEmployees");
+	xhr.send();
+}
