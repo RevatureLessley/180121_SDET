00c1cbc95530aacd9ca6290ad33d8e02695eeeab
Lynda Ademola <lyndaxj9@gmail.com>
2018-02-14 19:07:21 -0500

    fixed issue of numbers comparing as strings

--- Week4/Assignment/FizzBuzz/src/main/webapp/resources/javascript/fizzbuzz.js
+++ Week4/Assignment/FizzBuzz/src/main/webapp/resources/javascript/fizzbuzz.js
@@ -21,23 +21,34 @@ function fizzbuzz(x){
 }
 
 function checkValues() {
-	num1 = document.getElementById("number1").value;
-	num2 = document.getElementById("number2").value;
+	num1 = parseInt(document.getElementById("number1").value);
+	num2 = parseInt(document.getElementById("number2").value);
+	
+	if(document.getElementById("errormsg") != null){
+		document.getElementById("errormsg").remove();
+	}
 	
 	console.log("num1: " + num1 + " num2: " + num2);
 	
-	if(num1 == "" || num2 == ""){
-		console.log("1st if");
+	if(isNaN(num1) || isNaN(num2)){
+		console.log("a value is empty");
+		var h6 = document.createElement("h6");
+		h6.appendChild(document.createTextNode("Can't have empty values!"));
+		h6.setAttribute('id', 'errormsg');
+		h6.setAttribute('style', 'color:red;');
+		document.getElementById("question").appendChild(h6);
 		return false;
 	} else if(num1 < 0 || num1 > 100 || num2 < 0 || num2 > 100) {
 		console.log("2nd if");
 		return false;
 	} else {
+		console.log("b4 if| NUM1: " + num1 + "NUM2: " + num2);
 		if(num2 < num1) {
 			var tmp = num1;
 			num1 = num2;
 			num2 = tmp;
 		}
+		console.log("aft if| NUM1: " + num1 + "NUM2: " + num2);
 		return true;
 	}
 	
@@ -47,6 +58,7 @@ function generate() {
 	fizzbuzz();
 	
 	if(checkValues()){
+		console.log("after check| NUM1: " + num1 + "NUM2: " + num2);
 		document.getElementById("resulttable").innerHTML = "";
 		
 		var th1 = document.createElement("th");
@@ -60,7 +72,9 @@ function generate() {
 		row.appendChild(th2);
 		
 		document.getElementById("resulttable").appendChild(row);
+		console.log("after table| NUM1: " + num1 + "NUM2: " + num2);
 		for(i = num1; i <= num2; i++) {
+			console.log("NUM1: " + num1 + "NUM2: " + num2);
 			var td1 = document.createElement("td");
 			var td2 = document.createElement("td");
 			
@@ -74,11 +88,6 @@ function generate() {
 			document.getElementById("resulttable").appendChild(row);
 		}
 		
-	} else {
-		document.getElementById("resulttable").innerHTML = "";
-		var h2 = document.createElement("h2");
-		h2.appendChild(document.createTextNode("Please input valid numbers"));
-		document.getElementById("resultdiv").appendChild(h2);
 	}
 }
 
