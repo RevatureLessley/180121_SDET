a65cde7309db94fb3bea60fcbf96e289a128e714
Ryan Lessley <ryan.lessley@revature.com>
2018-02-15 15:01:32 -0500

    week 4

---
+++ Week4/InclassExamples/Week180121Angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { InterpolationComponent } from './components/interpolation/interpolation.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterpolationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
