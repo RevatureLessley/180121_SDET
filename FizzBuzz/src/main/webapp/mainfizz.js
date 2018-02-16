7ef29addd70595aac6249969fbcc104c9a3cc085
Richard Rosario <r.rosario2142@gmail.com>
2018-02-16 09:53:29 -0500

    fizzbuzz modifications

--- FizzBuzz/src/main/webapp/mainfizz.js
+++ FizzBuzz/src/main/webapp/mainfizz.js
@@ -6,9 +6,17 @@ function validate() {
 		document.getElementById("error").innerHTML = "<h2>Enter positive numbers up to 100, Preferably integers!</h2>";
 		return;
 	}
-	document.getElementById("error").innerHTML = "<h2>FIZZBUZZ!!!!!!!!!</h2>";
 	var x = parseInt(num1);
 	var y = parseInt(num2);
+	document.getElementById("error").innerHTML = "<h1>FIZZBUZZ!!!!</h1>";
+	document.getElementById("inputs").innerHTML = "<h1>WOAH!!! FIZZBUZZ!!!!</h1>";
+	var bText = document.createTextNode("AGAIN??");
+	var f = document.createElement('form'); //<form></form>
+	f.setAttribute('action', '/FizzBuzz/index.html'); //<form action="/index.html"></form>
+	var b = document.createElement('button'); //<button></button>
+	b.appendChild(bText); //<button>AGAIN??</button>
+	f.appendChild(b);
+	document.getElementById("inputs").appendChild(f);
 	fizzbuzz(x,y);
 }
 
@@ -27,8 +35,8 @@ function fizzbuzz(x,y) {
 	//fizzbuzz!!!!
 	for (var i = min; i <= max; i++) { 
 	    if (i%15 == 0) fizzBuzz = "FizzBuzz!!!";
-	    else if (i%3 == 0 && i%5 != 0) fizzBuzz = "Fizz!!!";
-	    else if(i%5 == 0 && i%3 != 0) fizzBuzz = "Buzz!!!";
+	    else if (i%3 == 0) fizzBuzz = "Fizz!!!";
+	    else if(i%5 == 0) fizzBuzz = "Buzz!!!";
 	    else fizzBuzz = i;
 	    var number = document.createTextNode(i); //Input
 		var fb = document.createTextNode(fizzBuzz); //Input
