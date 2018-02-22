6a4a58e2cba51828ff5a576fb66e454c05b68103
Richard Rosario <r.rosario2142@gmail.com>
2018-02-22 15:39:05 -0500

    db update

--- Projects/Project1/TRMS.sql
+++ Projects/Project1/TRMS.sql
@@ -8,13 +8,11 @@ CREATE TABLE personal_info( --parent table
     first_name VARCHAR2(100),
     last_name VARCHAR2(100),
     address VARCHAR2(100),
-    job_title VARCHAR2(100),
     join_date VARCHAR2(20)
 );
 
 CREATE TABLE account_info(
     email VARCHAR2(100) PRIMARY KEY,
-    username VARCHAR2(100),
     pw VARCHAR2(100),
     ben_co NUMBER(1),
     dir_sup NUMBER(1),
@@ -29,7 +27,8 @@ CREATE TABLE events(
     ben_co_email VARCHAR2(100),
     dir_sup_email VARCHAR2(100),
     approval_email VARCHAR2(100),
-    justification CLOB,
+    justification VARCHAR(300),
+    event_resource CLOB,
     grade VARCHAR2(2),
     pass_fail VARCHAR2(2),
     start_date VARCHAR2(20),
@@ -47,9 +46,6 @@ CREATE TABLE reimbursements(
     CONSTRAINT r_email_fk FOREIGN KEY (email) REFERENCES personal_info(email)
 );
 
-INSERT INTO reimbursements
-VALUES ('RRose@zmail.com', 0, 0, 0, 0, '25.Feb.1996');
-
 SELECT * FROM reimbursements;
 SELECT * FROM personal_info;
 SELECT * FROM account_info;
@@ -57,18 +53,18 @@ SELECT * FROM events;
 commit;
 
 INSERT INTO personal_info
-VALUES ('RRose@zmail.com', 'rich', 'rose', '1232', 'bloop', '25.Feb.1996');
+VALUES ('RRose@zmail.com', 'Rich', 'Rose', '123 Green St', '25.Feb.1996');
 INSERT INTO personal_info
-VALUES ('tto@zmail.com', 'treh', 'rse', '1lld232', 'bldf', '24.Mar.1991');
+VALUES ('Benny@kmail.com', 'Benny', 'Gualina', '244 Brown Rd', '24.Mar.1991');
 INSERT INTO account_info
-VALUES ('RRose@zmail.com', 'rich', 'rose', 0, 0, 0);
+VALUES ('RRose@zmail.com', 'password', 1, 0, 0);    --Benefits Coordinator
 INSERT INTO account_info
-VALUES ('tto@zmail.com', 'treh', 'rse', 0, 0, 0);
+VALUES ('Benny@kmail.com', 'wordpass', 0, 0, 0);
 INSERT INTO events
-VALUES ('RRose@zmail.com', 'kjhl', 'kkjkj', 'jhg', 'bloop', '25.Feb.1996', 'dff', 'f', 'd', 'dfgtres', 'fdswqa');
+VALUES ('RRose@zmail.com', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA');
 INSERT INTO events
-VALUES ('tto@zmail.com', 'kjhl', 'kkjkj', 'jhg', 'bloop', '25.Feb.1996', 'dff', 's', 'd', 'dfgtres', 'fdswqa');
+VALUES ('Benny@kmail.com', 'kjhl', 'kkjkj', 'jhg', 'bloop', 'NA', '25.Feb.1996', 'dff', 's', 'd', 'dfgtres', 'fdswqa');
 INSERT INTO reimbursements
 VALUES ('RRose@zmail.com', 3434, 4343, 3443, 3334, '25.Feb.1996');
 INSERT INTO reimbursements
-VALUES ('tto@zmail.com', 3424, 2344, 2343, 343, '24.Mar.1991');
+VALUES ('Benny@kmail.com', 3424, 2344, 2343, 343, '24.Mar.1991');
