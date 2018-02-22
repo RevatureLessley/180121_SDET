dbdbc6fc133891a28d521d66de3f7e5b4c2d7b8d
jawauntb <Juan@Jawauns-MBP.home>
2018-02-21 23:33:10 -0500

    project superspace

--- Week3/InClassExamples/180121_First_Maven/src/main/java/com/revature/main/Driver.java
+++
@@ -1,9 +0,0 @@
-package com.revature.main;
-
-public class Driver {
-
-	public static void main(String[] args) {
-		System.out.println("hello World");
-	}
-
-}
--- fizzBuzz/src/main/webapp/fizbus.js
+++ fizzBuzz/src/main/webapp/fizbus.js
@@ -5,26 +5,44 @@ src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"
 
 
 
-function getEm() {
+function getEm() {}
 	var firsty = document.getElementById("fir");
 	var lasty = document.getElementById("las");
 	
 	if(firsty > lasty){
-		temper = firsty;
+		temper = firsty; 
 		firsty = lasty;
 		lasty = temper;}
 	
 	if(firsty < 0 || lasty >100){
 		window.alert('value-INVALID');
 		return false;}
-	let count = lasty;
-	for(i = firsty; i <= count; i++){
-		var res = fizbuz(i);
+	for(i = firsty; i <= lasty; i++){
+		var tables=document.getElementById("tabf");
+		var res = fizzbuzz(i);
 		var nurow = document.createElement("tr");
 		var numcol = document.createElement("td");
 		var fizzcol = document.createElement("td");
 		var nums;
 		var fizz;
+		
+		function fizzbuzz(i){
+			l=(this.i % 3);
+			m=(this.i % 5);
+			n= (((Boolean(l)==false) && (Boolean(m)==false))  && i!=0);
+			if(n==true){	
+		    	fb = document.createTextNode("FizzBuzz"); 
+		    		return "fizzbuzz";
+		    	} else if ((Boolean(l)==false) && i!=0){				    		
+				return "fizz";			
+			} else if((Boolean(m)==false) && i!=0){				
+				return"buzz";	
+			} else if (this.i == null){
+				return Nan;
+			} else{
+				return i;
+			}
+		}
 		
 		nums = document.createTextNode(i);
 		fizz = document.createTextNode(res);
@@ -32,26 +50,12 @@ function getEm() {
 	  	fizzcol.appendChild(fizz);
 	  	nurow.appendChild(numcol);
 		nurow.appendChild(fizzcol);
-		document.getElementById("tabf").appendChild(nurow);
+		tables.insertRow(nurow);
 	}	
 }
 
 
-function fizbuz(fb1){
-	l=(this.fb1 % 3);
-	m=(this.fb1 % 5);
-	n= (((Boolean(l)==false) && (Boolean(m)==false))  && fb1!=0);
-	if(n==true){	
-    	fb = document.createTextNode("FizzBuzz"); 
-    		return "fizzbuzz";
-    	} else if ((Boolean(l)==false) && fb1!=0){				    		
-		return "fizz";			
-	} else if((Boolean(m)==false) && fb1!=0){				
-		return"buzz";	
-	} else if (this.fb1 == null){
-		return Nan;
-	} else{
-		return v;
-	}
-}
+
+
+
 
