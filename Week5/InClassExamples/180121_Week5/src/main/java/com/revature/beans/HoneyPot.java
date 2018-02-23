a02b0bc0d162e414155574c84d8548a53a7d485b
Ryan Lessley <ryan.lessley@revature.com>
2018-02-23 10:56:35 -0500

    week 5

---
+++ Week5/InClassExamples/180121_Week5/src/main/java/com/revature/beans/HoneyPot.java
@@ -0,0 +1,61 @@
+package com.revature.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="HONEYPOT")
+public class HoneyPot {
+	@Id
+	@Column(name="HONEYPOT_ID")
+	@SequenceGenerator(sequenceName="HP_SEQ", name="HP_SEQ")
+	@GeneratedValue(generator="HP_SEQ", strategy=GenerationType.SEQUENCE)
+	private Integer honeyPotId;
+	@Column
+	private double volume;
+	@Column
+	private double honeyAmount;
+	public Integer getHoneyPotId() {
+		return honeyPotId;
+	}
+	public void setHoneyPotId(Integer honeyPotId) {
+		this.honeyPotId = honeyPotId;
+	}
+	public double getVolume() {
+		return volume;
+	}
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+	public double getHoneyAmount() {
+		return honeyAmount;
+	}
+	public void setHoneyAmount(double honeyAmount) {
+		this.honeyAmount = honeyAmount;
+	}
+	public HoneyPot(Integer honeyPotId, double volume, double honeyAmount) {
+		super();
+		this.honeyPotId = honeyPotId;
+		this.volume = volume;
+		this.honeyAmount = honeyAmount;
+	}
+	public HoneyPot(double volume, double honeyAmount) {
+		super();
+		this.volume = volume;
+		this.honeyAmount = honeyAmount;
+	}
+	public HoneyPot() {
+
+	}
+	@Override
+	public String toString() {
+		return "HoneyPot [honeyPotId=" + honeyPotId + ", volume=" + volume + ", honeyAmount=" + honeyAmount + "]";
+	}
+	
+	
+}
