aef0ccc42118acb65960d3019eca51d5671feb11
Ryan Lessley <ryan.lessley@revature.com>
2018-02-28 10:40:13 -0500

    week 6

--- Week6/InClassExamples/180121_week6/src/test/java/com/revature/mercury/Driver.java
+++ Week6/InClassExamples/180121_week6/src/test/java/com/revature/mercury/Driver.java
@@ -10,6 +10,7 @@ public class Driver {
 	static String url = "http://newtours.demoaut.com/";
 	
 	public static void main(String[] args) throws InterruptedException {
+		//CHROME
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 
@@ -17,7 +18,8 @@ public class Driver {
 		//Starts the driver and opens it at the specified url./
 		driver.get(url);
 		
-		Thread.sleep(5000);
+		checkTitle("Welcome: Mercury Tours");
+		
 		
 		//This method closes the driver and is IMPERATIVE that you include it.
 		//Any time you run the application and dont invoke this method, leaves an hidden
@@ -25,6 +27,8 @@ public class Driver {
 		//task manager.
 		driver.quit();
 		
+		/*
+		 * FIREFOX
 		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 		driver = new FirefoxDriver();
 		
@@ -38,7 +42,25 @@ public class Driver {
 		//chrome process in teh background that you have to close manually via the
 		//task manager.
 		driver.quit();
-
+*/
+	}
+	
+	public static void checkTitle(String expected){
+		
+		String testString = "TEST " + ": ";
+		//A driver is any browser you are automating to run through your application.
+		//We call getTitle FROM the driver therefore.
+		String actual = driver.getTitle(); 
+		
+		
+		if(!actual.equals(expected)){
+			System.out.println(testString + "FAILED");
+			System.out.println("Expected: \'" + expected + "\'");
+			System.out.println("Actual:   \'" + actual + "\'");
+		}else{
+			System.out.println(testString + "PASSED");
+		}
+		
 	}
 
 }
---
+++ Week6/InClassExamples/180121_week6/src/test/java/com/revature/testng/NewTest.java
@@ -0,0 +1,89 @@
+package com.revature.testng;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/*
+ * With TestNG, we get access to more before/after options than our JUnit predecessor.
+ * 
+ * Should you utilize other classes witn the test file, the before/after class annotation
+ * will trigger. for each invocation of the contents.
+ * 
+ * Should you utilize multiple test classes within the same class file (IE, a suite)
+ * the before/after test annotations will trigger.
+ * 
+ * Should you utilize multiple suites, the before/after suite annotations will trigger.
+ * 
+ * Before//after method triggers for each @Test annotation.
+ * 
+ * Note: There is a 5th before/after annotation called before/after group.
+ * TestNg allows you to make custom group tags and apply them to groupings of methods.
+ * Before and after each specific group executes, these annotations trigger.
+ */
+
+public class NewTest {
+  @Test(groups={"group1"})
+  public void test1() {
+	  System.out.println("TEST1");
+	  
+  }
+  
+  @Test(groups={"group2"})
+  public void test3() {
+	  System.out.println("TEST3");
+	  
+  }
+  @Test(groups={"group1"})
+  public void test2() {
+	  System.out.println("TEST2");
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("BEFORE METHOD");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("AFTER METHOD");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("BEFORE CLASS");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("AFTER CLASS");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("BEFORE TEST");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("AFTER TEST");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("BEFORE SUITE");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("AFTER SUITE");
+  }
+
+}
